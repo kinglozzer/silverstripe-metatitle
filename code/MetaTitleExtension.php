3a3b41ceadd2d1d02a235e48a451ae424d40f66f
Loz Calver <lozcalver@bigfork.co.uk>
2013-04-15 10:36:19 +0100

    Initial commit

---
+++ code/MetaTitleExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+class MetaTitleExtension extends DataExtension {
+
+	private static $db = array(
+		'MetaTitle' => 'Varchar(255)'
+	);
+
+	public function updateCMSFields(FieldList $fields) {
+		$metaData = $fields->fieldByName('Root.Main.Metadata');
+
+		$metaFieldTitle = new TextField("MetaTitle", $this->owner->fieldLabel('MetaTitle'));
+		$metaFieldTitle->setRightTitle(
+				_t(
+					'SiteTree.METATITLEHELP',
+					'Shown at the top of the browser window and used as the "linked text" by search engines.'
+				)
+			)->addExtraClass('help');
+		
+		$metaData->insertBefore($metaFieldTitle, 'MetaDescription');
+
+		return $fields;
+	}
+
+	public function updateFieldLabels(&$labels) {
+		$labels['MetaTitle'] = _t('SiteTree.METATITLE', "Title");
+	}
+
+}
